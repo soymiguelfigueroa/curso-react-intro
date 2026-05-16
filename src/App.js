c871fa6504adb315d420cbc4f728ef5f3bef837f
Miguel Figueroa <miguelfigueroa90@gmail.com>
2026-05-15 23:25:12 -0400

    Primer componente

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +24,16 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem () {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
