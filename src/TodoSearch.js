c4ca42904ad9ee1bed4515262525a47031a2ffd2
Miguel Figueroa <miguelfigueroa90@gmail.com>
2026-05-20 06:57:45 -0400

    Imprimendo eventos de prueba

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,12 @@ import './TodoSearch.css';
 
 function TodoSearch () {
   return (
-    <input placeholder="Cortar cebolla" />
+    <input placeholder="Cortar cebolla" onChange={(event) => {
+      console.log('Escribiste en el TodoSearch');
+      console.log(event);
+      console.log(event.target);
+      console.log(event.target.value);
+    }} />
   );
 }
 
