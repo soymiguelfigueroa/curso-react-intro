1fc5a1779c7c58b42cb2e00dac7d4121b9ce12e8
Miguel Figueroa <miguelfigueroa90@gmail.com>
2026-06-19 07:53:49 -0400

    Cambiando a la versión 17

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './CreateTodoButton.css';
 import { TodoContext } from '../TodoContext';
+import { ReactComponent as CreateSVG } from './add.svg';
 
 function CreateTodoButton () {
   const {
@@ -14,7 +15,9 @@ function CreateTodoButton () {
         setOpenModal(true)
       }}
       >
-      <img src='icon-add.svg' alt="Icon add" />
+      <CreateSVG
+        alt={"Icon add"}
+      />
     </button>
   );
 }
